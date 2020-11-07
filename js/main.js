8ecdb8c17999bf629daa040e1f7dfb3dba1e05b9
alvikagal <alvikagal@gmail.com>
2020-11-07 11:05:07 +0200

    Исправил полоски

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 const burger = document.querySelector(".burger");
-var button = document.querySelector(".header_menu");
-// console.dir(menu);
-function burgerOn(){
-	// burger.classList.toggle("burger_active");
-	if(button.style.display == "block"){
-		button.style.display = "none";
-	}else{
-		button.style.display = "block";
+var navigation = document.querySelector(".header_menu");
+
+//отображать навигацию при изменении ширины экрана более 400рх
+onresize = function() {
+	if (document.documentElement.clientWidth > 400) {
+		navigation.style.display = "block";
+	} else {
+		navigation.style.display = "none";
 	}
 }
 
+//при нажатии на кнопку гамбургер показывать навигацию
+function burgerOn(){
+	if(navigation.style.display == "block"){
+		navigation.style.display = "none";
+	}else{
+		navigation.style.display = "block";
+	}
+}
+
+//при нажатии вне навигации - скрывать навигацию
 $(document).mouseup(function (e) {
-    var container = $(".burger");
-    var menu = $(".header_menu");
-    if (container.has(e.target).length === 0 && menu.has(e.target).length === 0 && button.style.display == "block"){
-        button.style.display = "none";
-    }
+	var container = $(".burger");
+	var menu = $(".header_menu");
+	if (container.has(e.target).length === 0 && menu.has(e.target).length === 0 && navigation.style.display == "block"){
+		navigation.style.display = "none";
+	}
 });
